c4d44a6e0d9445ea9fc2ba54dc89fda86d6c8d3b
Михаил Карпухин <mikekarpuhin@yandex.ru>
2023-11-07 01:28:58 +0000

    сделал приложение для диска

--- cub/projects/admin.py
+++ cub/projects/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 
-from projects.models import Project
+from projects.models import Project, Folder, ProjectFile
 
 
 @admin.register(Project)
-class UserPassportsAdmin(admin.ModelAdmin):
+class ProjectAdmin(admin.ModelAdmin):
     list_display = ('title', 'customer', 'leader', 'start_date', 'stop_date',)
+
+
+@admin.register(Folder)
+class FolderAdmin(admin.ModelAdmin):
+    list_display = ('name', 'project',)
+
+
+@admin.register(ProjectFile)
+class ProjectFileAdmin(admin.ModelAdmin):
+    list_display = ('name', 'project', 'folder', 'file')
--- cub/projects/models.py
+++ cub/projects/models.py
@@ -30,3 +30,31 @@ class Project(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Folder(models.Model):
+    """Модель папки"""
+    name = models.CharField(max_length=255, verbose_name="Название папки")
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="folders")
+
+    class Meta:
+        verbose_name_plural = 'Папки'
+        verbose_name = 'Папка'
+
+    def __str__(self):
+        return self.name
+
+
+class ProjectFile(models.Model):
+    """Модель файла проекта"""
+    name = models.CharField(max_length=255, verbose_name="Название файла")
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="files")
+    folder = models.ForeignKey(Folder, on_delete=models.SET_NULL, null=True, blank=True, related_name="files")
+    file = models.FileField(upload_to='project_files/', verbose_name="Файл")
+
+    class Meta:
+        verbose_name_plural = 'Файлы проекта'
+        verbose_name = 'Файл проекта'
+
+    def __str__(self):
+        return self.name
