07d59de1bf1e37c303958fec92c63f8895ddab29
Михаил Карпухин <mikekarpuhin@yandex.ru>
2023-11-09 23:22:47 +0000

    редактирование задач

--- cub/tasks/urls.py
+++ cub/tasks/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('api/v1/user/<int:user_id>/tasks/', views.UserTasksView.as_view(), name='user-all-tasks'),
     path('api/v1/user/<int:user_id>/task/<int:task_id>/', views.UserDetailTasksView.as_view(), name='user-detail-tasks'),
     path('api/v1/tasks/create/', views.TaskCreateView.as_view(), name='task-create'),
+    path('api/v1/tasks/delete/', views.TaskDeleteView.as_view(), name='task-delete'),
+    path('api/v1/tasks/<int:pk>/update/', views.TaskUpdateView.as_view(), name='task-update'),
 ]
--- cub/tasks/views.py
+++ cub/tasks/views.py
@@ -1,7 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import permissions
+from rest_framework import permissions, generics
 from rest_framework import status
+from rest_framework.decorators import api_view
 
 from .models import Task
 from .serializer import TaskFullSerializer, TaskDetailSerializer, TaskSerializer
@@ -19,6 +20,38 @@ class TaskCreateView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class TaskDeleteView(generics.DestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        task_ids = request.data.get('task_ids', [])
+        if not task_ids:
+            return Response({'detail': 'Задачи не выбраны.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        tasks_to_delete = Task.objects.filter(pk__in=task_ids)
+        if not tasks_to_delete.exists():
+            return Response({'detail': 'Задачи не найдены.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        tasks_to_delete.delete()
+        return Response({'detail': 'Задача(и) успешно удалены.'}, status=status.HTTP_204_NO_CONTENT)
+
+
+class TaskUpdateView(generics.UpdateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
 
 
 class UserTasksView(APIView):
