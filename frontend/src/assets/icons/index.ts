d8660c8e438411485374ddbc36770fbedcb635d6
mpa12 <maksim.28.05@mail.ru>
2023-11-10 21:33:10 +1000

    #8 fix: Изменена иконка проектов в меню

--- frontend/src/assets/icons/index.ts
+++ frontend/src/assets/icons/index.ts
@@ -3,12 +3,14 @@ import { ReactComponent as search } from './search.svg';
 import { ReactComponent as tasks } from './tasks.svg';
 import { ReactComponent as people } from './people.svg';
 import { ReactComponent as calendar } from './calendar.svg';
+import { ReactComponent as folder } from './folder.svg';
 
 export const ICONS = {
   search,
   tasks,
   people,
   calendar,
+  folder,
 };
 
 export function getIcon(key: keyof typeof ICONS, props: Record<string, any>) {
