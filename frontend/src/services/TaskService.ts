a1e04c3eff764a6c4724b15bcbccf48d394e6907
mpa12 <maksim.28.05@mail.ru>
2023-11-10 22:12:18 +1000

    fix: api.config.ts

--- frontend/src/services/TaskService.ts
+++ frontend/src/services/TaskService.ts
@@ -2,7 +2,7 @@ import { instance } from "../api.config";
 
 class TaskService {
   index() {
-    return instance.get('/tasks/api/v1/user/3/tasks/');
+    return instance.get('/tasks/api/v1/user/tasks/');
   }
 }
 
