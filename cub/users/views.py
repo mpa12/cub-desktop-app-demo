09bb62d991ab011714c6448407de270cc59c712d
Михаил Карпухин <mikekarpuhin@yandex.ru>
2023-11-11 18:16:53 +0000

    изменил сеиалайзер для пользователя

--- cub/users/views.py
+++ cub/users/views.py
@@ -1,18 +1,20 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import permissions, status
+from rest_framework import permissions
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from .models import User
 from .serializer import UserRoleSerializer
 
 
 class UserRoleView(APIView):
+    authentication_classes = [JWTAuthentication]
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self, user_id):
         return User.objects.filter(id=user_id)
 
-    def get(self, request, user_id):
-        user = self.get_queryset(user_id)
+    def get(self, request):
+        user = self.get_queryset(request.user)
         serializer = UserRoleSerializer(user, many=True)
         return Response(serializer.data)
