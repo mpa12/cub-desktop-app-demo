29f05f8b9c989e92ea94b7f08314c3dc826ae8dc
Михаил Карпухин <mikekarpuhin@yandex.ru>
2023-11-12 22:35:54 +0000

    исправил маршрут

--- cub/users/urls.py
+++ cub/users/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path('api/v1/user/identificate/', UserView.as_view(), name='user_view'),
+    path('api/v1/user/profile/', UserView.as_view(), name='user_view'),
 ]
