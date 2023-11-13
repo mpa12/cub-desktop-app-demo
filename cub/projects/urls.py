cbe2ca6c7abab7b6208cbd743dd25f0a91a44383
Михаил Карпухин <mikekarpuhin@yandex.ru>
2023-11-13 22:51:41 +0000

    список проектов

--- cub/projects/urls.py
+++ cub/projects/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/v1/projects/all/', views.ProjectView.as_view(), name='projects-all'),
+    path('api/v1/projects/', views.ProjectView.as_view(), name='projects-all'),
     path('api/v1/project/<int:project_id>/', views.ProjectDetailView.as_view(), name='projects-detail'),
 ]
