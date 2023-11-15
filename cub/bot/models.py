c56b8ad8d475ba630f678884f629acf844f6c24b
Михаил Карпухин <mikekarpuhin@yandex.ru>
2023-11-15 01:59:35 +0000

    бд для бота

--- cub/bot/models.py
+++ cub/bot/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class BotFAQ(models.Model):
+    """Модель для тг бота"""
+    question = models.CharField(max_length=255, verbose_name="Вопрос")
+    answer = models.CharField(max_length=255, verbose_name="Ответ", null=True, blank=True)
+    file = models.FileField(verbose_name="Файл", upload_to='bot_files/', null=True, blank=True)
