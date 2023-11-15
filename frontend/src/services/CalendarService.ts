670f21e89738a36dbe33470d6a8d9c37955758d2
mpa12 <maksim.28.05@mail.ru>
2023-11-16 01:30:41 +1000

    #25 Добавление и удаление событий (Необходимо перевести хранение событи на глобальный store)

--- frontend/src/services/CalendarService.ts
+++ frontend/src/services/CalendarService.ts
@@ -1,8 +1,36 @@
 import {instance} from "../api.config";
+import getCookie from "@utils/getCookie";
 
 class CalendarService {
   index() {
     return instance.get('/calendars/api/v1/user/events/');
+  }
+
+  create({
+    title,
+    description,
+    start_datetime,
+    bg_color,
+    text_color
+  }: {
+    title: string;
+    description: string;
+    start_datetime: string;
+    bg_color: string;
+    text_color: string;
+  }) {
+    instance.defaults.headers.common['X-CSRFToken'] = getCookie('csrftoken');
+    return instance.post('/calendars/api/v1/event/create/', {
+      title,
+      description,
+      start_datetime,
+      bg_color,
+      text_color
+    });
+  }
+
+  delete(eventId: number) {
+    return instance.post('/calendars/api/v1/events/delete/', { events_ids: eventId.toString() });
   }
 }
 
