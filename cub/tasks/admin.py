9a97ebfb3bea251ba7c173fd73612760a840fa7a
Михаил Карпухин <mikekarpuhin@yandex.ru>
2023-11-18 19:12:43 +0000

    админка

--- cub/tasks/admin.py
+++ cub/tasks/admin.py
@@ -6,3 +6,4 @@ from tasks.models import Task
 @admin.register(Task)
 class UserPassportsAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'status', 'project', 'time',)
+    exclude = ('start_time', )
