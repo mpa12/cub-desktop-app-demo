5819d966c5909912148c9bf78beae873291fa552
Михаил Карпухин <mishan2003@bk.ru>
2023-11-29 03:03:04 +1000

    Создание и просмотр папки к проекту

--- cub/projects/urls.py
+++ cub/projects/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('api/v1/projects/', views.ProjectView.as_view(), name='projects-all'),
     path('api/v1/project/<int:project_id>/', views.ProjectDetailView.as_view(), name='projects-detail'),
+    path('api/v1/project/<int:project_id>/folders/create/', views.FolderCreateView.as_view(), name='create_folder'),
+    path('api/v1/project/<int:project_id>/folders/', views.FolderView.as_view(), name='view-folders'),
 ]
--- cub/projects/views.py
+++ cub/projects/views.py
@@ -1,10 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import permissions
+from rest_framework import permissions, status
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
-from .models import Project
-from .serializer import ProjectSerializer
+from .models import Project, Folder
+from .serializer import ProjectSerializer, FolderSerializer
 
 
 class ProjectView(APIView):
@@ -30,4 +30,38 @@ class ProjectDetailView(APIView):
     def get(self, request, project_id):
         project = self.get_queryset(project_id)
         serializer = ProjectSerializer(project, many=True)
+        return Response(serializer.data)
+
+
+class FolderCreateView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    queryset = Folder.objects.none()
+
+    def post(self, request, project_id):
+        # Проверяем существование проекта
+        try:
+            project = Project.objects.get(id=project_id)
+        except Project.DoesNotExist:
+            return Response({'error': 'Проект не найден'}, status=status.HTTP_404_NOT_FOUND)
+
+        # Сериализуем данные из запроса
+        serializer = FolderSerializer(data=request.data)
+        if serializer.is_valid():
+            # Сохраняем папку, связанную с проектом
+            folder = serializer.save(project=project)
+            return Response(FolderSerializer(folder).data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class FolderView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self, project_id):
+        return Folder.objects.filter(project_id=project_id)
+
+    def get(self, request, project_id):
+        folders = self.get_queryset(project_id)
+        serializer = FolderSerializer(folders, many=True)
         return Response(serializer.data)
