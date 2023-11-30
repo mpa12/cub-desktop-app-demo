f0911693abdbb98cae80e706f9b75ab7566489b1
to-be-peace-da <nikita.nik.u@mail.ru>
2023-11-30 22:53:57 +1000

    #31 Проекты и favicon

--- frontend/src/components/projects/Folder.tsx
+++ frontend/src/components/projects/Folder.tsx
@@ -3,17 +3,23 @@ import Icon from "@ui/Icon";
 import cn from "classnames";
 
 interface FolderProps {
-  name: string;
+  data: {
+    id: number;
+    name: string;
+  };
 }
 
 const folderClassName = cn('flex items-center flex-col rounded cursor-pointer',
   'p-[5px] hover:bg-gray-hover self-start max-w-[100px]');
 
-const Folder = ({name}: FolderProps) => {
+const Folder = ({data}: FolderProps) => {
+
+
+
   return (
     <div className={folderClassName}>
       <Icon iconName={'folderImg'} className={'w-[50px]'}/>
-      <p className={'text-[12px] text-center'}>{name}</p>
+      <p className={'text-[12px] text-center'}>{data.name}</p>
     </div>
   )
 };
